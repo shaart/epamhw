c7405455f6aa596fa61669e792ab7ecd3bf299c6
Artur <imshaart@gmail.com>
2018-05-13 18:25:31 +0300

    Added @ToString to Value

--- task1/src/main/java/com/epam/model/Value.java
+++ task1/src/main/java/com/epam/model/Value.java
@@ -9,12 +9,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 @XmlRootElement(name = "value")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Value implements Serializable {
