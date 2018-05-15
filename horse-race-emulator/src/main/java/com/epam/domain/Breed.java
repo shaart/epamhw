99d5956e181527daa5680c725347446b768e5483
Shalaev Artur <imshaart@gmail.com>
2018-05-15 21:58:30 +0300

    Entities implemented

--- horse-race-emulator/src/main/java/com/epam/domain/Breed.java
+++ horse-race-emulator/src/main/java/com/epam/domain/Breed.java
@@ -1,5 +1,19 @@
 package com.epam.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Breed {
 
+  private String name;
 }
--- horse-race-emulator/src/main/java/com/epam/domain/Horse.java
+++ horse-race-emulator/src/main/java/com/epam/domain/Horse.java
@@ -1,5 +1,24 @@
 package com.epam.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Horse {
 
+  private Rider rider;
+  private Breed breed;
+  private String name;
+
+  @Override
+  public String toString() {
+    return String.format("[%s] %s (%s)", name, rider.getName(), breed.getName());
+  }
 }
