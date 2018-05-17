80b2ab78ddbf6b57bb7bc9a898aecd263ad98777
Shalaev Artur <imshaart@gmail.com>
2018-05-17 15:49:03 +0300

    Menu options moved to Enum, code refactored

--- horse-race-emulator/src/main/java/com/epam/Application.java
+++ horse-race-emulator/src/main/java/com/epam/Application.java
@@ -3,7 +3,6 @@ package com.epam;
 import com.epam.domain.Horse;
 import com.epam.domain.Race;
 import com.epam.services.EmulationService;
-import com.epam.services.HorseService;
 import com.epam.services.RaceService;
 import java.util.List;
 import java.util.Scanner;
@@ -16,10 +15,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @Setter
 public class Application {
 
+  enum MenuOptions {HORSE_NAME, BREED_NAME, RIDER_NAME, UNKNOWN}
+
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 
-    HorseService horseService = (HorseService) context.getBean("horseService");
     RaceService raceService = (RaceService) context.getBean("raceService");
     EmulationService emulationService = (EmulationService) context.getBean("emulationService");
 
@@ -31,29 +31,30 @@ public class Application {
     Horse bet = null;
     Scanner scanner = new Scanner(System.in);
 
-    final int HORSE_NAME_OPTION = 1;
-    final int BREED_NAME_OPTION = 2;
-    final int RIDER_NAME_OPTION = 3;
-
     final String BET_MENU_FORMAT = "Make your bet.Select horse by:%n%d.Name%n%d.Breed%n%d.Rider's name%n%n";
-    do {
+
+    while (bet == null) {
       log.info(
-          String.format(BET_MENU_FORMAT, HORSE_NAME_OPTION, BREED_NAME_OPTION, RIDER_NAME_OPTION));
-      int option = getUserOption(scanner);
+          String.format(BET_MENU_FORMAT,
+              MenuOptions.HORSE_NAME.ordinal(),
+              MenuOptions.BREED_NAME.ordinal(),
+              MenuOptions.RIDER_NAME.ordinal())
+      );
+      MenuOptions option = getUserOption(scanner);
 
       log.info("Search: ");
       String input = scanner.nextLine();
-
+      log.info("Searching for " + input + "...");
       Horse foundHorse = null;
       switch (option) {
-        case HORSE_NAME_OPTION:
-          foundHorse = horseService.findByName(input);
+        case HORSE_NAME:
+          foundHorse = raceService.findByName(currentRace, input);
           break;
-        case BREED_NAME_OPTION:
-          foundHorse = horseService.findByBreed(input);
+        case BREED_NAME:
+          foundHorse = raceService.findByBreed(currentRace, input);
           break;
-        case RIDER_NAME_OPTION:
-          foundHorse = horseService.findByRiderName(input);
+        case RIDER_NAME:
+          foundHorse = raceService.findByRiderName(currentRace, input);
           break;
         default:
           log.error("Unknown option");
@@ -65,7 +66,7 @@ public class Application {
       } else {
         log.info("Horse not found");
       }
-    } while (bet == null);
+    } // while
 
     Horse winner = emulationService.emulate(currentRace);
     log.info("Winner: " + winner.toString());
@@ -82,20 +83,37 @@ public class Application {
     }
   }
 
-  private static int getUserOption(Scanner scanner) {
-    final int UNSELECTED = -1;
-    int option = UNSELECTED;
-    do {
+  private static MenuOptions getUserOption(Scanner scanner) {
+    MenuOptions option = MenuOptions.UNKNOWN;
+
+    while (option == MenuOptions.UNKNOWN) {
       log.info("Enter option: ");
       String input = scanner.nextLine();
+      log.info("Got option: " + input);
       try {
-        option = Integer.parseInt(input);
+        int userOption = Integer.parseInt(input);
+        option = parseMenuOption(userOption);
+        if (option == MenuOptions.UNKNOWN) {
+          log.info("Unknown option");
+        }
       } catch (Exception e) {
         log.info("Incorrect input");
         scanner.nextLine();
       }
-    } while (option == UNSELECTED);
+    }
 
+    return option;
+  }
+
+  private static MenuOptions parseMenuOption(int userOption) {
+    MenuOptions option = MenuOptions.UNKNOWN;
+
+    for (MenuOptions menuOption : MenuOptions.values()) {
+      if (menuOption.ordinal() == userOption) {
+        option = menuOption;
+        break;
+      }
+    }
     return option;
   }
 }
